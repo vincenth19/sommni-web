43dc7fa7ca20f147a57c86c77d1e32534ae72740
nirmalkarmani <nirmalkarmani@gmail.com>
2022-04-04 20:16:22 +0700

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ms", "zh", "zh-TW", "hi"],
     localeDetection: false,
+    localePath: path.resolve("./public/locales"),
   },
 };
