62eaa47f7addfd00d101a8f6ddf1cf9fb1337fb3
nirmalkarmani <nirmalkarmani@gmail.com>
2022-04-04 19:40:45 +0700

    Fixed build errors to allow for Vercel deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -167,7 +167,7 @@ const SleepExperienceSection: FC<SleepExperienceSectionProps> = ({
   return (
     <Box style={{ padding: "1rem 0" }}>
       <h1 style={{ textAlign: "center", padding: "2rem 0" }}>
-        Sommni's Guaranteed Sleep Experience
+        Sommni&apos;s Guaranteed Sleep Experience
       </h1>
       <div
         style={{
